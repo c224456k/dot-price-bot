841752b2f6d95273a7ae4d103e37528e9114e59c
c224456k <81842601+c224456k@users.noreply.github.com>
2025-12-19 22:27:19 +0800

    Update DCbot.py

--- DCbot.py
+++ DCbot.py
@@ -2,7 +2,7 @@ import requests
 
 def get_dot_data():
     # 使用 CoinGecko API 取得現在價格與 24h 漲跌
-    crypto_url = "https://discord.com/api/webhooks/1331621312405176372/t-u1AXN1WtUYGCqP9XjaiB_aHsL1AyTxTdz9OKGRPDxM48WYRcd9B0S7Dz3aaMGx5VLy"
+    crypto_url = "https://api.coingecko.com/api/v3/simple/price?ids=polkadot&vs_currencies=usd&include_24hr_change=true"
     
     try:
         response = requests.get(crypto_url)
@@ -42,7 +42,7 @@ def send_to_discord(webhook_url, current, yesterday, change):
     requests.post(webhook_url, json=payload)
 
 # --- 設定區 ---
-MY_WEBHOOK_URL = "https://discord.com/api/webhooks/1451566651765162036/z7-pOpZ0DKtodgdV8n9pGEFX-NVIohsqlSt4EQAL2LebGsOY9-7eO_Fvgy2zawcTXjc1"
+MY_WEBHOOK_URL = "https://discord.com/api/webhooks/1331621312405176372/t-u1AXN1WtUYGCqP9XjaiB_aHsL1AyTxTdz9OKGRPDxM48WYRcd9B0S7Dz3aaMGx5VLy"
 
 # 執行
 cur, yes, chg = get_dot_data()
@@ -50,3 +50,4 @@ if cur is not None:
     send_to_discord(MY_WEBHOOK_URL, cur, yes, chg)
 
     print(f"成功發送！現在: ${cur:.2f}, 一天前: ${yes:.2f}")
+
